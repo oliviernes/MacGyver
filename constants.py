e3c04b3133d5127b797005f5b8ca6523082c9676
oliviernes <partialoues@gmail.com>
2019-12-22 01:55:58 +0100

    add requirements and constants files

---
+++ constants.py
@@ -0,0 +1,20 @@
+"""Constants needed for Mc Giver Labyrinth game"""
+
+#Window parameter:
+SPRITES_NUMBER = 15
+SPRITE_SIZE = 30
+WINDOW_SIZE = SPRITES_NUMBER * SPRITE_SIZE
+
+# #Window customization:
+# titre_fenetre = "DK Labyrinthe"
+# image_icone = "images/dk_droite.png"
+
+#Images list:
+ETHER_IMAGE = "sources/images/ether.png"
+FLOOR_IMAGE = "sources/images/floor1.png"
+GARDIAN_IMAGE = "sources/images/guardian.png"
+MACGYVER_IMAGE = "sources/images/macgyver.png"
+NEEDLE_IMAGE = "sources/images/needle.png"
+ROD_IMAGE = "sources/images/rod.png"
+WALL_IMAGE = "sources/images/wall.png"
+
